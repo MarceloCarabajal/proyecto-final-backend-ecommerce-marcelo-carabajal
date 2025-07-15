b993cbb0d9bad631dc02b4b8c0ed8f63c5e1726b
MarceloCarabajal <marce.tsgia@gmail.com>
2025-07-14 23:54:26 -0300

    Creacion de variables de entorno para url del frontend

--- src/main/java/com/mcarabajal/talentotech/configuration/WebConfiguration.java
+++ src/main/java/com/mcarabajal/talentotech/configuration/WebConfiguration.java
@@ -1,18 +1,23 @@
 package com.mcarabajal.talentotech.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfiguration {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
